875baebf25e4f60c119cc6949a12aa49c877e6b1
小飼慎一 <kogai0121@gmail.com>
2017-05-02 21:03:01 +0900

    Spec for a bit complicated variant type

--- src/webhook.rs
+++ src/webhook.rs
@@ -4,32 +4,30 @@ use iron::status;
 use serde_json;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct MessageText {
-  #[serde(rename = "type")]
-  message_type: String,
-  id: String,
-  text: String,
-}
-
-#[derive(Debug)]
-enum Messages {
-    Text(MessageText),
-    // Sticky,
+#[serde(tag = "type")]
+pub enum Message {
+    #[serde(rename = "text")]
+    Text { id: String, text: String },
+    
+    #[serde(rename_all = "camelCase")]
+    #[serde(rename = "sticker")]
+    Sticker { id: String, package_id: String, sticker_id: String },
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct SourceGroup {
-  #[serde(rename = "type")]
-  source_type: String,
-  group_id: String,
-}
-
-#[derive(Debug)]
+#[serde(tag = "type")]
 pub enum Source {
-  Group(SourceGroup),
-  Room,
-  User,
+  #[serde(rename_all = "camelCase")]
+  #[serde(rename = "group")]
+  Group { group_id: String },
+
+  #[serde(rename_all = "camelCase")]
+  #[serde(rename = "room")]
+  Room { room_id: String },
+  
+  #[serde(rename_all = "camelCase")]
+  #[serde(rename = "user")]
+  User { user_id: String },
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
@@ -39,8 +37,8 @@ pub struct Event {
   event_type: String,
   reply_token: String,
   timestamp: isize,
-  source: SourceGroup,
-  message: MessageText,
+  source: Source,
+  message: Message,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
@@ -58,44 +56,84 @@ pub fn index_post(req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "{}")))
 }
 
-fn parse_request_body(s: &String) -> Event {
-  serde_json::from_str::<Event>(s).unwrap()
+fn parse_request_body(s: &String) -> WebHook {
+  serde_json::from_str::<WebHook>(s).unwrap()
 }
 
 mod tests {
   use super::*;
 
   #[test]
-  fn it_should_parse_body() {
+  fn it_should_parse_user_group() {
       let result = parse_request_body(&r#"
-        {
-            "type": "message",
-            "replyToken": "reply-token",
-            "source": {
-              "groupId": "group-id",
-              "type": "group"
-            },
-            "timestamp": 1493644768038,
-            "message": {
-              "type": "text",
-              "id": "message-id",
-              "text": "sample text"
-            }
+      {
+        "events": [{
+          "type": "message",
+          "replyToken": "reply-token",
+          "source": {
+            "groupId": "group-id",
+            "type": "group"
+          },
+          "timestamp": 1493644768038,
+          "message": {
+            "type": "text",
+            "id": "message-id",
+            "text": "sample text"
           }
+        }]   
+      } 
       "#.to_owned());
-      assert_eq!(result, Event {
-        event_type: "message".to_owned(),
-        reply_token: "reply-token".to_owned(),
-        timestamp: 1493644768038,
-        source: SourceGroup {
-          source_type: "group".to_owned(),
-          group_id: "group-id".to_owned(),
-        },
-        message: MessageText {
-          message_type: "text".to_owned(),
-          id: "message-id".to_owned(),
-          text: "sample text".to_owned(),
-        },
+      assert_eq!(result, WebHook {
+        events: vec![Event {
+          event_type: "message".to_owned(),
+          reply_token: "reply-token".to_owned(),
+          timestamp: 1493644768038,
+          source: Source::Group {
+            group_id: "group-id".to_owned(),
+          },
+          message: Message::Text {
+            id: "message-id".to_owned(),
+            text: "sample text".to_owned(),
+          },
+        }]
+      });
+  }
+
+  #[test]
+  fn it_should_parse_sticker_message() {
+      let result = parse_request_body(&r#"
+      {
+        "events": [{
+          "type": "message",
+          "replyToken": "reply-token",
+          "source": {
+            "roomId": "room-id",
+            "type": "room"
+          },
+          "timestamp": 1493644768038,
+          "message": {
+            "type": "sticker",
+            "id": "message-id",
+            "packageId": "package-id",
+            "stickerId": "sticker-id"
+          }
+        }]   
+      } 
+      "#.to_owned());
+      assert_eq!(result, WebHook {
+        events: vec![Event {
+          event_type: "message".to_owned(),
+          reply_token: "reply-token".to_owned(),
+          timestamp: 1493644768038,
+          source: Source::Room {
+            room_id: "room-id".to_owned(),
+          },
+          message: Message::Sticker {
+            id: "message-id".to_owned(),
+            package_id: "package-id".to_owned(),
+            sticker_id: "sticker-id".to_owned(),
+          },
+        }]
       });
   }
 }
